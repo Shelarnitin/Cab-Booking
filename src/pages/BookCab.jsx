03f30fed2173b740fc02c34d3be77759acb4dfd8
Shelarnitin <shelarnitin399@gmail.com>
2025-10-18 12:26:51 +0530

    booking cabs

---
+++ src/pages/BookCab.jsx
@@ -0,0 +1,8 @@
+export default function BookCab() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-semibold mb-4">Book a New Cab</h1>
+      <p className="text-gray-600">Here you can create a new booking form (coming next).</p>
+    </div>
+  );
+}

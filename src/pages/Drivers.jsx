6d86b918128872f56161c97f48e54b53c21c3657
Shelarnitin <shelarnitin399@gmail.com>
2025-10-18 12:27:38 +0530

    for drivers

---
+++ src/pages/Drivers.jsx
@@ -0,0 +1,8 @@
+export default function Drivers() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-semibold mb-4">Drivers</h1>
+      <p className="text-gray-600">This page will show all available drivers and their assigned trips.</p>
+    </div>
+  );
+}

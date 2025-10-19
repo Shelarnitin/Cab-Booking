5c5a30a1e58582b3d004ad901c79d70fb795d942
Shelarnitin <shelarnitin399@gmail.com>
2025-10-19 11:41:02 +0530

    layout added

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+export default function Layout() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}

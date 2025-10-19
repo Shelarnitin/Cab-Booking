6969752c5e7808769df409ecd19bd87e45946e6b
Shelarnitin <shelarnitin399@gmail.com>
2025-10-19 11:50:13 +0530

    update for showing login & dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard.jsx";
-import BookCab from './pages/BookCab.jsx'
-import Bookings from "./pages/Bookings.jsx";
-import Drivers from "./pages/Drivers.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/book" element={<BookCab />} />
-        <Route path="/bookings" element={<Bookings />} />
-        <Route path="/drivers" element={<Drivers />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );

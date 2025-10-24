11df8350475d76c16ec950f7be4f99fa2ff2ce8b
Shelarnitin <shelarnitin399@gmail.com>
2025-10-24 19:52:25 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from './pages/Dashboard';
 import BookCab from './pages/BookCab';
 import Bookings from './pages/Bookings';
 import Drivers from './pages/Drivers';
+import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
   return (
@@ -15,13 +16,41 @@ export default function App() {
 
       {/* protected/app routes that share navbar */}
       <Route path="/" element={<Layout />}>
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="book" element={<BookCab />} />
-        <Route path="bookings" element={<Bookings />} />
-        <Route path="drivers" element={<Drivers />} />
+        <Route
+          path="dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="book"
+          element={
+            <ProtectedRoute>
+              <BookCab />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="bookings"
+          element={
+            <ProtectedRoute>
+              <Bookings />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="drivers"
+          element={
+            <ProtectedRoute>
+              <Drivers />
+            </ProtectedRoute>
+          }
+        />
       </Route>
 
-      {/* fallback: redirect unknown paths to login */}
+      {/* fallback route */}
       <Route path="*" element={<Login />} />
     </Routes>
   );

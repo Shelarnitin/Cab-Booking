151c40e7eb64568e5883f27fad969e8d18e0ea07
Shelarnitin <shelarnitin399@gmail.com>
2025-10-26 16:45:49 +0530

    booking

---
+++ backend/controllers/bookingController.js
@@ -0,0 +1,20 @@
+import Booking from "../models/bookingModel.js";
+
+export const createBooking = async (req, res) => {
+  try {
+    const booking = new Booking(req.body);
+    await booking.save();
+    res.status(201).json(booking);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const getBookings = async (req, res) => {
+  try {
+    const bookings = await Booking.find();
+    res.json(bookings);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};

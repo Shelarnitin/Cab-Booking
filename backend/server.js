a7901d7247a7dce0d1c9e62319515f2a98aa7df3
Shelarnitin <shelarnitin399@gmail.com>
2025-10-26 16:47:35 +0530

    server

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect MongoDB
+connectDB();
+
+// Routes
+app.use("/api/bookings", bookingRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));

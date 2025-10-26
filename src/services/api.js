771c4832cb6354e2c9cd77d5b0ba6f0e09895e70
Shelarnitin <shelarnitin399@gmail.com>
2025-10-26 17:21:19 +0530

    api updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  baseURL: "http://localhost:5000/api", // backend base URL
 });
 
+// Create new booking
 export const createBooking = (data) => API.post("/bookings", data);
+
+// Fetch all bookings
 export const getBookings = () => API.get("/bookings");

b1845df2ad0ae419c0a5c956d30d60b7ec17859d
Shelarnitin <shelarnitin399@gmail.com>
2025-10-28 15:07:54 +0530

    added view & add Driver to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -19,10 +19,16 @@ export default function Dashboard() {
           <button onClick={() => navigate('/bookings')} className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg">View Bookings</button>
         </div>
 
+        
         <div className="bg-white p-6 rounded-xl shadow">
-          <h2 className="text-orange-600 text-lg font-semibold mb-2">Drivers</h2>
+          <h2 className="text-orange-600 text-lg font-semibold mb-2">AddDriver</h2>
           <p className="text-gray-600 mb-4">View available drivers and their assigned trips.</p>
-          <button onClick={() => navigate('/drivers')} className="bg-orange-600 hover:bg-orange-700 text-white px-4 py-2 rounded-lg">Manage Drivers</button>
+          <button onClick={() => navigate('/add-driver')} className="bg-orange-600 hover:bg-orange-700 text-white px-4 py-2 rounded-lg">Manage Drivers</button>
+        </div>
+        <div className="bg-white p-6 rounded-xl shadow">
+          <h2 className="text-purple-600 text-lg font-semibold mb-2">ViewDriver</h2>
+          <p className="text-gray-600 mb-4">View available drivers and their assigned trips.</p>
+          <button onClick={() => navigate('/viewdrivers')} className="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded-lg">Manage Drivers</button>
         </div>
       </div>
     </div>
